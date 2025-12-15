7871ae93d194ce71b764da00c40430e9d18f306b
patrici06 <patriciorivero77@gmail.com>
2025-12-15 19:36:46 -0300

    back y front

--- Proyecto/migration.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO mibd.roles (nombre) VALUES ('USUARIO'), ('ADMINISTRADOR');
-INSERT INTO mibd.estados_usuario (nombre) VALUES ('HABILITADO'), ('DESHABILITADO');
-INSERT INTO mibd.tamanios_mascota (nombre) VALUES ('PEQUENIO'), ('MEDIANO'), ('GRANDE');
--- Proyecto/src/main/java/ttps/proyecto/models/enums/EstadoMascota.java
+++
@@ -1,9 +0,0 @@
-package ttps.proyecto.models.enums;
-
-//El único Enum del sistema.
-public enum EstadoMascota {
-    PERDIDO_PROPIO,
-    PERDIDO_AJENO,
-    RECUPERADO,
-    ADOPTADO
-}
