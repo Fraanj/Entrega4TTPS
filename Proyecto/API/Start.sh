4fcea5f249a6a1adc7544cf07d6d45826550868a
patrici06 <patriciorivero77@gmail.com>
2025-12-16 22:32:39 -0300

    automatizacion de bd

--- Proyecto/API/Start.sh
+++ Proyecto/API/Start.sh
@@ -2,7 +2,9 @@
 echo "Principio Run"
 sudo mvn clean package -DskipTests 
 sudo mv ./target/Proyecto.war /opt/tomcat/webapps
+echo "archivo Movido"
 sudo /opt/tomcat/bin/startup.sh 
+echo "tomcat startup"
 cd ../Front/DondeEstas/
 ng serve
 echo "Fin run"
