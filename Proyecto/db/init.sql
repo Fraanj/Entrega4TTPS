a48f63103b4f3009e196f1c587450d9ddeb6f489
patricio rivero <patriciorivero77@gmail.com>
2026-01-15 14:28:10 -0300

    actualizacion del readme

---
+++ Proyecto/db/init.sql
@@ -0,0 +1,3 @@
+INSERT INTO mibd.roles (nombre) VALUES ('USUARIO'), ('ADMINISTRADOR');
+INSERT INTO mibd.estados_usuario (nombre) VALUES ('HABILITADO'), ('DESHABILITADO');
+INSERT INTO mibd.tamanios_mascota (nombre) VALUES ('PEQUENIO'), ('MEDIANO'), ('GRANDE');
